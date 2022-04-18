bc52fb5abcd2bfb31123f453e4f84be168bff64b
Vitalij202020 <vaskouski.v.v@gmail.com>
2022-04-18 16:50:32 +0300

    done

--- server.js
+++ server.js
@@ -3,9 +3,25 @@ const mongoose = require('mongoose')
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017", (err) => {
+mongoose.connect("mongodb://localhost:27017/test", (err) => {
     !err ? console.log("db connected!!!") : console.log("db error")
 })
+
+async function start() {
+    const schema = new mongoose.Schema({
+        name: String,
+        age: Number
+    })
+
+    const model = new mongoose.model("Users", schema)
+
+    const data = new model({name: "Vitya", age: 30})
+    console.log(data.name)
+    const answer = await data.save()
+    console.log(answer)
+}
+
+start()
 
 
 const PORT = process.env.PORT || 5000;
